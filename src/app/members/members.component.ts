058e73b8fd4b002ef3936fd8fc6a56ec6589d6e0
togo-mentor <uint2435@gmail.com>
2021-12-11 14:23:21 +0900

    コンポーネントの読み込み

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembersComponent implements OnInit {
 
+  member = '田中太郎'
+
   constructor() { }
 
+  // コンポーネントが初期化されるときに実行
   ngOnInit(): void {
   }
 
